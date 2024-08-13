8deafda9bc204b16ba747701f8c2a95ec6418fea
Mohammed Sobhani <syedmahmoud41@gmail.com>
2024-08-13 19:24:13 +0600

    project build done

---
+++ scripts.js
@@ -0,0 +1,25 @@
+let paraNumb = document.getElementById("para-value");
+let generateBtn = document.getElementById("generate-btn");
+let ipsumPara = document.getElementById("ipsum-para");
+let ipsumUrl = "https://api.api-ninjas.com/v1/loremipsum?paragraphs="
+let apiKey = "Aqh0jbrjF4JgB+eRvwQ42w==kU27velBtycUf8jd";
+let copyText = document.getElementById("copy-text");
+
+
+function ipsumGenerator(){
+    config = {
+        method:"GET",
+        headers: { 'X-Api-Key': apiKey}
+    }
+    fetch(ipsumUrl+paraNumb.value,config)
+    .then(res=>res.json())
+    .then(result => ipsumPara.innerText = result.text)
+    .catch(err=>console.log(err));
+}
+copyText.addEventListener("click",()=>{
+    // to copy text to the clipboard
+    navigator.clipboard.writeText(ipsumPara.innerText);
+})
+
+generateBtn.addEventListener("click",ipsumGenerator);
+window.addEventListener("load",ipsumGenerator); 
